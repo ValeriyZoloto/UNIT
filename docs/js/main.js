3d4d3eda044e83b23c5f7b791f0169fab092805a
Valeriy <leshikzoloto@gmail.com>
2020-08-30 17:05:25 +0300

    First commit

---
+++ docs/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        navSpeed: 1000,
+        smartSpeed: 1000,
+        autoplaySpeed: 1000
+        // navText: ['', '']
+    });
+});
+
